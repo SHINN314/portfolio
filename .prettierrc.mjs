bc018e086039ca81e50747d02a6f9602a151dab7
SHINN314 <shinnosukekimura8@gmail.com>
2025-02-09 21:21:59 +0900

    Revert "prettierの設定終了"
    This reverts commit e83377b85cdc6924dca0df0441f5db3163c8c36c.

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,16 +1,15 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
-	printWidth: 120,
-	plugins: ["prettier-plugin-astro"],
-	overrides: [
-		{
-			files: "*.astro",
-			options: {
-				parser: "astro",
-			},
-		},
-	],
-	useTabs: true,
-	semi: true,
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+  useTabs: true,
+  semi: true,
 };
