65555464cc2fe4c4852c5e585a62e4cea04995bb
SHINN314 <shinnosukekimura8@gmail.com>
2025-03-29 03:01:08 +0900

    よくわかんないけどドキュメント通りconfigファイルを設定した

---
+++ src/content/config.ts
@@ -0,0 +1,11 @@
+// 1. `astro:content`からユーティリティをインポート
+import { defineCollection } from "astro:content";
+// 2. コレクションを定義
+const blogCollection = defineCollection({
+	/* ... */
+});
+// 3. コレクションを登録するために、単一の`collections`オブジェクトをエクスポート
+//    このキーは、"src/content"のコレクションのディレクトリ名と一致する必要があります。
+export const collections = {
+	blog: blogCollection,
+};
